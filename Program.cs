20cd3cc2eea59e801d769f4673868a2fa0ff8ee8
Snehasrg <sghongade642@gmail.com>
2023-03-20 00:10:38 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -7,12 +7,26 @@ namespace Demo
         public static void Main(String[] args)
         {
 
-            ConstructorDemo obj= new ConstructorDemo(10,"Raj");
-            Console.WriteLine(obj.age);
-            Console.WriteLine(obj.name);
+            //ConstructorDemo obj= new ConstructorDemo(10,"Raj");
+            //Console.WriteLine(obj.age);
+            //Console.WriteLine(obj.name);
 
-            Dog dog = new Dog();  
-            dog.animalSound();
+            //Dog dog = new Dog();  
+            //dog.animalSound();
+
+        // Method call for Access Modifiers
+            AccessModifierDemo person = new AccessModifierDemo();
+            Console.WriteLine(person.id);//created object for public class
+           // Console.WriteLine(person.name);//created object for private class
+            Console.WriteLine(person.RollNumber);//created object for internal class
+
+           // Console.WriteLine(person.username);//created object for protected class
+            Employee employee = new Employee();
+            employee.Print(); 
+
+
+
+
 
         }
     }
