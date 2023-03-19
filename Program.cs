d96beb2075c56839c01280dee8dddb7183370f9a
Snehasrg <sghongade642@gmail.com>
2023-03-19 15:11:43 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Demo
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            //Add a= new Add();
+            //Console.WriteLine(a.id);
+
+            //    Add a1 = new Add(7);
+            //    Console.WriteLine(a1.id);
+
+           // ConstructorDemo obj= new ConstructorDemo();
+            //Console.WriteLine(obj.age);
+
+            Dog dog = new Dog();  
+            dog.animalSound();
+
+
+
+        }
+    }
+}
