77269431cbbef471c378e3af6441f89631c9dafe
Snehasrg <sghongade642@gmail.com>
2023-03-20 00:09:50 +0530

    Added Access Modifiers types

---
+++ AccessModifierDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Demo
+{
+    public class AccessModifierDemo
+    {
+        public int id = 1;
+        private string name = "Raj";
+        internal int RollNumber = 7;
+        protected string username = "Raj@";
+    }
+    public class Employee : AccessModifierDemo//inherited class for protected access modifier
+    {
+        public void Print()
+        {
+            Console.WriteLine(username);
+        }
+    }
+}
+
+      
+       
+    
