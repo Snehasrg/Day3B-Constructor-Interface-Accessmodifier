5d979461a6b6077565818431127fcd07eb230a3a
Snehasrg <sghongade642@gmail.com>
2023-03-19 15:18:31 +0530

    Different types of constructor

---
+++ ConstructorDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class ConstructorDemo
+    {
+       public int age;
+        public string name;
+        public ConstructorDemo() 
+        {
+            age =70;
+        }
+        public ConstructorDemo(int x)
+        {
+            age = x;
+            
+        }
+        public ConstructorDemo(int x ,string y)
+        {
+            age = x;
+            name = y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,19 +6,10 @@ namespace Demo
     {
         public static void Main(String[] args)
         {
-            //Add a= new Add();
-            //Console.WriteLine(a.id);
 
-            //    Add a1 = new Add(7);
-            //    Console.WriteLine(a1.id);
-
-           // ConstructorDemo obj= new ConstructorDemo();
-            //Console.WriteLine(obj.age);
-
-            Dog dog = new Dog();  
-            dog.animalSound();
-
-
+            ConstructorDemo obj= new ConstructorDemo(10,"Raj");
+            Console.WriteLine(obj.age);
+            Console.WriteLine(obj.name);
 
         }
     }
